ed1d03f489629085a6623f49217d2ee87de89698
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-03-29 15:45:06 +0100

    Methods are bindedto module

--- android/src/de/appwerft/mp3agic/Mp3agicModule.java
+++ android/src/de/appwerft/mp3agic/Mp3agicModule.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
@@ -35,10 +36,8 @@ import ti.modules.titanium.filesystem.FileProxy;
 import org.appcelerator.kroll.common.Log;
 import org.appcelerator.kroll.common.TiConfig;
 
-
-@Kroll.module(name="Mp3agic", id="de.appwerft.mp3agic")
-public class Mp3agicModule extends KrollModule
-{
+@Kroll.module(name = "Mp3agic", id = "de.appwerft.mp3agic")
+public class Mp3agicModule extends KrollModule {
 
 	// Standard Debugging variables
 	private static final String LCAT = "Mp3agicModule";
@@ -47,25 +46,21 @@ public class Mp3agicModule extends KrollModule
 	// You can define constants with @Kroll.constant, for example:
 	// @Kroll.constant public static final String EXTERNAL_NAME = value;
 
-	public Mp3agicModule()
-	{
+	public Mp3agicModule() {
 		super();
 	}
 
 	@Kroll.onAppCreate
-	public static void onAppCreate(TiApplication app)
-	{
-		Log.d(LCAT, "inside onAppCreate");
-		// put module init code that needs to run when the application is created
+	public static void onAppCreate(TiApplication app) {
 	}
 
 	public static TiBaseFile getTiBaseFileFromInput(Object readPath) {
-		if (readPath==null) {
+		if (readPath == null) {
 			Log.e(LCAT, "parameter must be a string (path) or a file object");
 			return null;
 		}
-		
-		Log.d(LCAT,readPath.getClass().getName());
+
+		Log.d(LCAT, readPath.getClass().getName());
 		TiBaseFile inputFile = null;
 		try {
 			if (readPath instanceof TiFile) {
@@ -104,56 +99,56 @@ public class Mp3agicModule extends KrollModule
 		}
 		return null;
 	}
-	
+
 	public static Mp3File getID3fromMP3File(TiBaseFile inputFile) {
-		Log.d(LCAT,inputFile.toString());
-		Log.d(LCAT,inputFile.getNativeFile().getAbsolutePath());
-		
+		Log.d(LCAT, inputFile.toString());
+		Log.d(LCAT, inputFile.getNativeFile().getAbsolutePath());
+
 		try {
 			return new Mp3File(inputFile.getNativeFile());
-			
+
 		} catch (UnsupportedTagException | InvalidDataException | IOException e) {
 			Log.e(LCAT, e.getMessage());
 			return null;
 		}
 	}
-	
+
 	@Kroll.method
 	public KrollDict getId3Tag(Object arg) {
 		TiBaseFile inputFile = getTiBaseFileFromInput(arg);
 		Mp3File mp3file = getID3fromMP3File(inputFile);
-		if (mp3file==null) return null;
+		if (mp3file == null)
+			return null;
 		KrollDict dict = new KrollDict();
-		if (mp3file == null) return null;
-			dict.put("bitrate", mp3file.getBitrate());
-			dict.put("channelmode", mp3file.getChannelMode());
-			dict.put("emphasis", mp3file.getEmphasis());
-			dict.put("endoffset", mp3file.getEndOffset());
-			dict.put("filename", mp3file.getFilename());
-			dict.put("framecount", mp3file.getFrameCount());
-			dict.put("lastmodified", mp3file.getLastModified());
-			dict.put("layer", mp3file.getLayer());
-			dict.put("length", mp3file.getLength());
-			dict.put("lengthsinseconds", mp3file.getLengthInSeconds());
-			dict.put("modeextension", mp3file.getModeExtension());
-			dict.put("samplerate", mp3file.getSampleRate());
-			dict.put("startoffset", mp3file.getStartOffset());
-			dict.put("xingbitrate", mp3file.getXingBitrate());
-			dict.put("xingoffset", mp3file.getXingOffset());
-			dict.put("iscopyright", mp3file.isCopyright());
-			dict.put("isoriginal", mp3file.isOriginal());
-			dict.put("isvbr", mp3file.isVbr());
-			dict.put("hasid3v1tag", mp3file.hasId3v1Tag());
-			dict.put("hasid3v2tag", mp3file.hasId3v2Tag());
-			return dict;
+		dict.put("bitrate", mp3file.getBitrate());
+		dict.put("channelmode", mp3file.getChannelMode());
+		dict.put("emphasis", mp3file.getEmphasis());
+		dict.put("endoffset", mp3file.getEndOffset());
+		dict.put("filename", mp3file.getFilename());
+		dict.put("framecount", mp3file.getFrameCount());
+		dict.put("lastmodified", mp3file.getLastModified());
+		dict.put("layer", mp3file.getLayer());
+		dict.put("length", mp3file.getLength());
+		dict.put("lengthsinseconds", mp3file.getLengthInSeconds());
+		dict.put("modeextension", mp3file.getModeExtension());
+		dict.put("samplerate", mp3file.getSampleRate());
+		dict.put("startoffset", mp3file.getStartOffset());
+		dict.put("xingbitrate", mp3file.getXingBitrate());
+		dict.put("xingoffset", mp3file.getXingOffset());
+		dict.put("iscopyright", mp3file.isCopyright());
+		dict.put("isoriginal", mp3file.isOriginal());
+		dict.put("isvbr", mp3file.isVbr());
+		dict.put("hasid3v1tag", mp3file.hasId3v1Tag());
+		dict.put("hasid3v2tag", mp3file.hasId3v2Tag());
+		return dict;
 	}
-	
-	
+
 	@Kroll.method
 	public KrollDict getId3v1Tag(Object arg) {
 		TiBaseFile inputFile = getTiBaseFileFromInput(arg);
 		Mp3File mp3file = getID3fromMP3File(inputFile);
-		if (mp3file==null) return null;
+		if (mp3file == null)
+			return null;
 		KrollDict dict = new KrollDict();
 		if (mp3file == null)
 			return null;
@@ -166,20 +161,21 @@ public class Mp3agicModule extends KrollModule
 			dict.put("year", id3v1Tag.getYear());
 			dict.put("genre", id3v1Tag.getGenre());
 			dict.put("description", id3v1Tag.getGenreDescription());
-			
+
 			dict.put("comment", id3v1Tag.getComment());
 			return dict;
 		}
 		return null;
 	}
 
-	
+	@SuppressWarnings("deprecation")
 	@Kroll.method
 	public KrollDict getId3v2Tag(Object arg) {
 		TiBaseFile inputFile = getTiBaseFileFromInput(arg);
 		Mp3File mp3file = getID3fromMP3File(inputFile);
-		if (mp3file==null) return null;
-		
+		if (mp3file == null)
+			return null;
+
 		if (mp3file.hasId3v2Tag()) {
 			KrollDict dict = new KrollDict();
 			ID3v2 tag = mp3file.getId3v2Tag();
@@ -190,8 +186,8 @@ public class Mp3agicModule extends KrollModule
 			dict.put("year", tag.getYear());
 			dict.put("genre", tag.getGenre());
 			dict.put("description", tag.getGenreDescription());
-			dict.put("comment", tag.getComment());
-			dict.put("lyrics", tag.getLyrics());
+			dict.put("comment",  StringEscapeUtils.unescapeHtml4(tag.getComment()));
+			dict.put("lyrics", StringEscapeUtils.unescapeHtml4(tag.getLyrics()));
 			dict.put("composer", tag.getComposer());
 			dict.put("publisher", tag.getPublisher());
 			dict.put("originalartist", tag.getOriginalArtist());
@@ -204,12 +200,12 @@ public class Mp3agicModule extends KrollModule
 		return null;
 	}
 
-	
 	@Kroll.method
 	public String getAlbumimage(Object arg) {
 		TiBaseFile inputFile = getTiBaseFileFromInput(arg);
 		Mp3File mp3file = getID3fromMP3File(inputFile);
-		if (mp3file==null) return null;
+		if (mp3file == null)
+			return null;
 		if (!mp3file.hasId3v2Tag())
 			return null;
 		ID3v2 tag = mp3file.getId3v2Tag();
@@ -217,7 +213,7 @@ public class Mp3agicModule extends KrollModule
 		String[] parts = mime.split("/");
 		String suffix = ".png";
 		if (parts.length > 1)
-			suffix = "."+parts[1];
+			suffix = "." + parts[1];
 		File temp;
 		try {
 			temp = File.createTempFile("albumimage", suffix, TiApplication.getInstance().getCacheDir());
@@ -231,7 +227,7 @@ public class Mp3agicModule extends KrollModule
 				try {
 					if (fos != null) {
 						fos.close();
-						return "file://"+temp.getAbsolutePath();
+						return "file://" + temp.getAbsolutePath();
 
 					}
 				} catch (IOException ioe) {
@@ -245,6 +241,5 @@ public class Mp3agicModule extends KrollModule
 		return null;
 
 	}
-	
+      
 }
-
