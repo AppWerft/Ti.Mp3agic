d855bca77f7e13e49feecb3f2e0f9c4b2926f687
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-04-24 07:50:58 +0200

    new lib 0.9.1

---
+++ android/build/generated/bootstrap.js
@@ -0,0 +1,46 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ *
+ * Warning: This file is GENERATED, and should not be modified
+ */
+var bootstrap = kroll.NativeModule.require("bootstrap"),
+	invoker = kroll.NativeModule.require("invoker"),
+	Titanium = kroll.binding("Titanium").Titanium;
+
+function moduleBootstrap(moduleBinding) {
+	function lazyGet(object, binding, name, namespace) {
+		return bootstrap.lazyGet(object, binding,
+			name, namespace, moduleBinding.getBinding);
+	}
+
+	var module = moduleBinding.getBinding("de.appwerft.mp3agic.Mp3agicModule")["Mp3agic"];
+	var invocationAPIs = module.invocationAPIs = [];
+	module.apiName = "Mp3agic";
+
+	function addInvocationAPI(module, moduleNamespace, namespace, api) {
+		invocationAPIs.push({ namespace: namespace, api: api });
+	}
+
+	addInvocationAPI(module, "Mp3agic", "Mp3agic", "createAlbumImage");
+		if (!("__propertiesDefined__" in module)) {Object.defineProperties(module, {
+"AlbumImage": {
+get: function() {
+var AlbumImage =  lazyGet(this, "de.appwerft.mp3agic.AlbumImageProxy", "AlbumImage", "AlbumImage");
+return AlbumImage;
+},
+configurable: true
+},
+
+});
+module.constructor.prototype.createAlbumImage = function() {
+return new module["AlbumImage"](arguments);
+}
+}
+module.__propertiesDefined__ = true;
+return module;
+
+}
+exports.bootstrap = moduleBootstrap;
---
+++ android/build/generated/jni/de.appwerft.mp3agic.AlbumImageProxy.cpp
@@ -0,0 +1,121 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2018 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "de.appwerft.mp3agic.AlbumImageProxy.h"
+
+#include "AndroidUtil.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+#include "org.appcelerator.titanium.proxy.TiViewProxy.h"
+
+#define TAG "AlbumImageProxy"
+
+using namespace v8;
+
+namespace de {
+namespace appwerft {
+namespace mp3agic {
+	namespace mp3agic {
+
+
+Persistent<FunctionTemplate> AlbumImageProxy::proxyTemplate;
+jclass AlbumImageProxy::javaClass = NULL;
+
+AlbumImageProxy::AlbumImageProxy() : titanium::Proxy()
+{
+}
+
+void AlbumImageProxy::bindProxy(Local<Object> exports, Local<Context> context)
+{
+	Isolate* isolate = context->GetIsolate();
+
+	Local<FunctionTemplate> pt = getProxyTemplate(isolate);
+
+	v8::TryCatch tryCatch(isolate);
+	Local<Function> constructor;
+	MaybeLocal<Function> maybeConstructor = pt->GetFunction(context);
+	if (!maybeConstructor.ToLocal(&constructor)) {
+		titanium::V8Util::fatalException(isolate, tryCatch);
+		return;
+	}
+
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "AlbumImage"); // use symbol over string for efficiency
+	exports->Set(context, nameSymbol, constructor);
+}
+
+void AlbumImageProxy::dispose(Isolate* isolate)
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Reset();
+	}
+
+	titanium::TiViewProxy::dispose(isolate);
+}
+
+Local<FunctionTemplate> AlbumImageProxy::getProxyTemplate(v8::Isolate* isolate)
+{
+	Local<Context> context = isolate->GetCurrentContext();
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate.Get(isolate);
+	}
+
+	LOGD(TAG, "AlbumImageProxy::getProxyTemplate()");
+
+	javaClass = titanium::JNIUtil::findClass("de/appwerft/mp3agic/AlbumImageProxy");
+	EscapableHandleScope scope(isolate);
+
+	// use symbol over string for efficiency
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "AlbumImage");
+
+	Local<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(
+		isolate,
+		titanium::TiViewProxy::getProxyTemplate(isolate),
+		javaClass,
+		nameSymbol);
+
+	proxyTemplate.Reset(isolate, t);
+	t->Set(titanium::Proxy::inheritSymbol.Get(isolate), FunctionTemplate::New(isolate, titanium::Proxy::inherit<AlbumImageProxy>));
+
+	// Method bindings --------------------------------------------------------
+
+	Local<ObjectTemplate> prototypeTemplate = t->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = t->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return scope.Escape(t);
+}
+
+Local<FunctionTemplate> AlbumImageProxy::getProxyTemplate(v8::Local<v8::Context> context)
+{
+	return getProxyTemplate(context->GetIsolate());
+}
+
+// Methods --------------------------------------------------------------------
+
+// Dynamic property accessors -------------------------------------------------
+
+
+	} // namespace mp3agic
+} // mp3agic
+} // appwerft
+} // de
